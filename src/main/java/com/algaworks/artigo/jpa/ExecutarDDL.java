df15c93264e657f9309a9e557665d2de347c004f
Alexandre Afonso <afonsoaaf@gmail.com>
2019-06-19 02:12:57 -0300

    Ajuste.

---
+++ src/main/java/com/algaworks/artigo/jpa/ExecutarDDL.java
@@ -0,0 +1,27 @@
+package com.algaworks.artigo.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.Persistence;
+
+public class ExecutarDDL {
+
+	public static void main(String... string) {
+		Map<String, String> properties = new HashMap<>();
+		
+		properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost/sistemaerp?createDatabaseIfNotExist=true");
+		properties.put("javax.persistence.jdbc.user", "root");
+		properties.put("javax.persistence.jdbc.password", "123");
+		
+		properties.put("javax.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
+		
+		properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+		
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		
+		Persistence.generateSchema("GerarDDL-PU", properties);
+	}
+}
---
+++ src/main/java/com/algaworks/artigo/jpa/ExportarDDL.java
@@ -0,0 +1,22 @@
+package com.algaworks.artigo.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.Persistence;
+
+public class ExportarDDL {
+
+	public static void main(String... string) {
+		Map<String, String> properties = new HashMap<>();
+
+		properties.put("javax.persistence.schema-generation.scripts.action", "drop-and-create");
+		properties.put("javax.persistence.schema-generation.scripts.create-target", "/tmp/ddl/script-criacao.sql");
+		properties.put("javax.persistence.schema-generation.scripts.drop-target", "/tmp/ddl/script-remocao.sql");
+		
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+		properties.put("hibernate.format_sql", "true");
+		
+		Persistence.generateSchema("GerarDDL-PU", properties);
+	}
+}
